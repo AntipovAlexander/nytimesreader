758d456245b9804751f81e032644a088fefb90e9
aleksandrantipov <antipov@themindstudios.com>
2022-04-23 19:10:40 +0300

    implemented details screen

--- news_details/src/main/java/ny/times/reader/news_details/presentation/NewsDetailsUI.kt
+++ news_details/src/main/java/ny/times/reader/news_details/presentation/NewsDetailsUI.kt
@@ -1,11 +1,19 @@
 package ny.times.reader.news_details.presentation
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import ny.times.reader.base.R
 import ny.times.reader.base.presentation.ui.widget.CollapsingToolbarScaffold
@@ -40,7 +48,45 @@ fun NewsDetails(details: NewsDetailsData) {
             )
         },
         content = {
-            // todo
+            Column(
+                modifier = Modifier
+                    .padding(all = 16.dp)
+                    .fillMaxWidth()
+                    .defaultMinSize(minHeight = LocalConfiguration.current.screenHeightDp.dp)
+            ) {
+                Text(
+                    text = details.abstract,
+                    style = TimesReaderTheme.typography.arcticTitle
+                )
+                Text(
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    text = generateByLine(details),
+                    style = TimesReaderTheme.typography.H3Regular,
+                    color = TimesReaderTheme.colors.grey
+                )
+                Text(
+                    text = details.leadParagraph,
+                    style = TimesReaderTheme.typography.H1Regular
+                )
+                TextButton(
+                    onClick = {
+                        // todo
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Continue reading on source ...",
+                        color = TimesReaderTheme.colors.black,
+                        style = TimesReaderTheme.typography.H1Regular
+                    )
+                }
+            }
         }
     )
+}
+
+fun generateByLine(details: NewsDetailsData): String {
+    val byline = "By: ${details.sourceName}, "
+    val tags = "tags: " + details.tags.joinToString(separator = ", ")
+    return byline + tags
 }
