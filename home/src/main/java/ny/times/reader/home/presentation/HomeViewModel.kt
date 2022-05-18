b1b4f40381c189f2c04eaa9736e5adec08e639b1
aleksandrantipov <antipov@themindstudios.com>
2022-05-18 23:08:07 +0300

    added navigator for tabs

--- home/src/main/java/ny/times/reader/home/presentation/HomeViewModel.kt
+++ home/src/main/java/ny/times/reader/home/presentation/HomeViewModel.kt
@@ -1,9 +1,6 @@
 package ny.times.reader.home.presentation
 
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import ny.times.reader.base.presentation.view_model.BaseViewAction
 import ny.times.reader.base.presentation.view_model.BaseViewModel
 import ny.times.reader.home.navigation.BottomTabs
@@ -15,19 +12,7 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
     private val homeNavigation: HomeNavigation,
     private val homeRouter: HomeRouter
-) : BaseViewModel<HomeViewState>(HomeViewState.INITIAL) {
-
-
-    init {
-        observeRouteChanges()
-    }
-
-    private fun observeRouteChanges() {
-        homeRouter
-            .routes
-            .onEach { sendAction(HomeViewActions.ChangeCurrentRoute(it.route)) }
-            .launchIn(viewModelScope)
-    }
+) : BaseViewModel<HomeViewState>(HomeViewState.INITIAL), HomeRouter by homeRouter {
 
     override fun onReduceState(viewAction: BaseViewAction): HomeViewState = when (viewAction) {
         is HomeViewActions.ChangeCurrentRoute -> state.copy(currentRoute = viewAction.route)
--- home/src/main/java/ny/times/reader/home/presentation/ui/HomeUi.kt
+++ home/src/main/java/ny/times/reader/home/presentation/ui/HomeUi.kt
@@ -6,12 +6,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
 import ny.times.reader.base.domain.entity.News
 import ny.times.reader.home.navigation.NavigationGraph
 import ny.times.reader.home.presentation.HomeViewModel
+import ny.times.reader.navigator.home.HomeRoutes
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
@@ -20,6 +26,13 @@ fun Home(
     onNewsClicked: (News) -> Unit
 ) {
     val homeNavController = rememberAnimatedNavController()
+
+    LaunchedEffect(homeViewModel) {
+        homeViewModel
+            .routes
+            .onEach { route -> navigate(homeNavController, route) }
+            .collect()
+    }
 
     Scaffold(
         bottomBar = {
@@ -40,19 +53,18 @@ fun Home(
         )
     }
 
-    // todo : navigation here
+}
 
-    //                    homeNavController.navigate(it.route) {
-//                        // Pop up to the start destination of the graph to
-//                        // avoid building up a large stack of destinations
-//                        // on the back stack as users select items
-//                        popUpTo(homeNavController.graph.findStartDestination().id) {
-//                            saveState = true
-//                        }
-//                        // Avoid multiple copies of the same destination when
-//                        // reselecting the same item
-//                        launchSingleTop = true
-//                        // Restore state when reselecting a previously selected item
-//                        restoreState = true
-//                    }
+private fun navigate(navController: NavController, homeRoute: HomeRoutes) {
+    navController.navigate(homeRoute.route) {
+        // Pop up to the start destination of the graph to
+        // avoid building up a large stack of destinations
+        // on the back stack as users select items
+        popUpTo(navController.graph.findStartDestination().id) { saveState = true }
+        // Avoid multiple copies of the same destination when
+        // reselecting the same item
+        launchSingleTop = true
+        // Restore state when reselecting a previously selected item
+        restoreState = true
+    }
 }
